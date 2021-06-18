f6c4073ec4ca4dab68fc9e040960610c5f913831
Raphael <raphael.mandich@gmail.com>
2021-06-17 22:52:43 -0300

    Finished chapter 5

--- dplyr_basics.R
+++ dplyr_basics.R
@@ -5,7 +5,7 @@ library(tidyverse)
 flights
 
 #Filter shows a subset of the data based on your restrictions
-filter(.data = flights, month == 1, day == 1)
+filter(data = flights, month == 1, day == 1)
 
 filter(flights, month == 11 | month == 12)
 
@@ -55,3 +55,114 @@ select(flights, -(year:day))
 
 #rename is pretty obvious
 rename(flights, tail_num = tailnum)
+
+#mutate add new columns that are functions of other colunms
+#first We create a new smaller dataset
+
+flights_sml <- select(flights, 
+                      year:day, 
+                      ends_with("delay"), 
+                      distance, 
+                      air_time
+                      )
+
+#Then we can add new calculated columns to it, using mutate
+
+mutate(flights_sml,
+       gain = dep_delay - arr_delay,
+       speed = distance / air_time * 60
+       )
+
+mutate(flights_sml,
+       gain = dep_delay - arr_delay,
+       hours = air_time / 60,
+       gain_per_hour = gain / hours
+       )
+
+#Transmute keeps only the new variables
+
+transmute(flights,
+          gain = dep_delay - arr_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain / hours
+          )
+
+#Exercises
+#Changing dep_time to a format that's more useful
+
+transmute(flights,
+          dep_time,
+          hour = dep_time %/% 100,
+          minute = dep_time %% 100,
+          minutes_since_midnight = hour * 60 + minute
+          )
+
+#comparing air_time, with arr_time - dep_time
+
+transmute(flights,
+          air_time,
+          difference = arr_time - dep_time)
+
+#comparing dep_time, sched_dep_time and dep_delay
+
+transmute(flights,
+          dep_time,
+          sched_dep_time,
+          dep_delay)
+
+#Summarise is used to collapse a data frame into a single row, 
+#containing a statistical expression. When combined with group by
+#we can make statistical aggregations of variables, like this:
+
+summarise(flights, delay = mean(dep_delay, na.rm = TRUE))
+
+#The na.rm argument is used to remove NA values
+
+by_day <- group_by(flights, year, month, day)
+summarise(by_day, delay = mean(dep_delay, na.rm = TRUE))
+
+
+#The 'pipe' operator (%>%) is very useful to perform multiple
+#transformations with data in the same expression. A good way
+#to think about the pipe is to read it as "Then"
+
+#This code attributes the data frame to a variable, THEN it groups
+#by destination, THEN it summarises some measures and THEN it applies
+#filter to remove unwanted data.
+
+delays <- flights %>% 
+        group_by(dest) %>% 
+        summarise(
+                count = n(),
+                dist = mean(distance, na.rm = TRUE),
+                delay = mean(arr_delay, na.rm = TRUE)
+                ) %>% 
+        filter(count > 20, dest != "HNL")
+
+ggplot(data = delays, mapping = aes(x = dist, y = delay)) +
+        geom_point(aes(size = count), alpha = 1/3) +
+        geom_smooth(se = FALSE)
+
+#Changing to a dataset that contains information about baseball
+
+library(Lahman)
+
+batting <- as_tibble(Lahman::Batting)
+
+#Group by PlayerID and measure the batting average(sum of hits divided
+#by attempts) and the number of attempts
+batters <- batting %>% 
+        group_by(playerID) %>% 
+        summarise(
+                ba = sum(H, na.rm = TRUE) / sum(AB, na.rm = TRUE),
+                ab = sum(AB, na.rm = TRUE)
+                )
+
+#Filters for players with more than 100 attempts and plots
+
+batters %>% 
+        filter(ab > 100) %>% 
+        ggplot(mapping = aes(x = ab, y = ba)) +
+        geom_point() + 
+        geom_smooth(se = FALSE)
+       
